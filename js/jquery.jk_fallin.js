e5e6f5adb3cac228a77db8acc61ef24308a13512
jangkae <jangkae@gmail.com>
2015-05-27 22:01:02 +0900

    base complete

--- js/jquery.jk_fallin.js
+++ js/jquery.jk_fallin.js
@@ -8,37 +8,47 @@ $.fn.jk_fallin = function(opts){
 	
 	return this.each(function(idx,o){
 		var $cont = $(o);
-		var itemWidth = getItemWidth();
-		var itemHeight = getItemHeight();
 		var $items = $cont.find(options.itemElem+':not(.default_item)');
-		var containerWidth = $cont.width();
-		var mat = [];
-		mat.push ( getEmptyColumn(itemWidth , containerWidth) );
 
+		var gridWidth = getGridWidth();
+		var gridHeight = getGridHeight();
+		var containerWidth = $cont.width();
+
+		// window resize 에 재실행.
 		if ( !$cont.data('jk_fallin.initialized') ) {
 			$cont.data('jk_fallin.initialized', true);
 			$cont.data('jk_fallin.oldWidth', $cont.width());
 			$(window).bind('resize.jk_fallin',function(){
 				// 컨테이너 width가 기존과 다르면 실행.
-				if ( $cont.data('oldWidth') != $cont.width() ) {
+				if ( $cont.data('jk_fallin.oldWidth') != $cont.width() ) {
 					console.log ( '실행' );
-					$cont.jk_fallin();
-					$cont.data('oldWidth', $cont.width() );
+					$cont.jk_fallin(options);
+					$cont.data('jk_fallin.oldWidth', $cont.width() );
 				}
 			});
 		}
 
-		$items.each(function(idx,o){
-			var $item = $(o);
-			//정렬 할 요소가 없으면 return;
-			if ( !$item.length ) return;
-			//현재 아이템이 matrix상 몇 칸을 차지 할 것인지 판단.
-			var w = Math.ceil($item.width() / itemWidth); 
-			var h = Math.ceil($item.height() / itemHeight);
-			var tx, ty, isPos = false; //투입가능한 상태저장
+		//css 설정
+		$cont.css('position', 'relative');
+		$items.css('position', 'absolute')
 
-			// matrix 첫번째 루프
-			for ( var i = 0 ; i < mat.length ; i++ ) {
+		//정사각형일때
+		if ( options.type == 'square' ) {
+			var mat = [];
+			mat.push ( getEmptyRow(gridWidth , containerWidth) );
+
+			$items.each(function(idx,o){
+				var $item = $(o);
+				//정렬 할 요소가 없으면 return;
+				if ( !$item.length ) return;
+
+				//현재 아이템이 matrix상 몇 칸을 차지 할 것인지 판단.
+				var w = Math.ceil($item.width() / gridWidth); 
+				var h = Math.ceil($item.height() / gridHeight);
+				var tx, ty, isPos = false; //투입가능한 상태저장
+
+				// matrix 첫번째 루프
+				for ( var i = 0 ; i < mat.length ; i++ ) {
 					// matrix 두번째 루프
 					for ( var j = 0 ; j < mat[i].length ; j++ ){
 
@@ -76,29 +86,70 @@ $.fn.jk_fallin = function(opts){
 					if ( isPos ) break;
 				}
 
+				// 차지할 자리가 없으면 끝에 추가하기위한 변수 설정
 				if ( !isPos ) {
 					i = mat.length;
 					j = 0;
 				}
 
-				//실제로 matrix에 추가.
+				// matrix에 추가.
 				for ( var l = 0 ; l < w ; l++ ) {
 					for ( var m = 0 ; m < h ; m++ ) {
-						if ( !mat[m+i] ) mat.push(getEmptyColumn(itemWidth,containerWidth));
+						if ( !mat[m+i] ) mat.push(getEmptyRow(gridWidth,containerWidth));
 						mat[i+m][j+l] = 1;
 					}
 				}
 
-				$item.stop().css({'left':j*itemWidth,'top':i*itemHeight});
+				$item.data({
+					'jk_fallin.tx':j*(gridWidth+options.marginWidth) + getMarginValue(),
+					'jk_fallin.ty':i*(gridHeight+options.marginHeight)
+				});
+
 				/*
 				if (!du)$(this).stop().css({'left':j*tileWidth,'top':i*tileHeight});
 				else $(this).stop().animate({'left':j*tileWidth,'top':i*tileHeight},{duration:du, easing:'easeInOutCubic'});
 				*/
 
-		});
-		
+				//컨테이너 Height 컨트롤
+				if ( options.contianerHeightControl ) {
+					$cont.height( mat.length * (gridHeight+options.marginHeight)-options.marginHeight);
+				}
+			});
+			// item each 끝.
+		} // 정사각형 일때 끝.
+		//직사각형일때
+		else if ( options.type == 'rectangle' ) {
+			var mat = getEmptyRow();
+
+			$items.each(function(i,o){
+				var $item = $(o);
+				//target column 요소가 들어갈 자리.
+				var tc = getMinIndex(mat);
+				$item.data({
+					'jk_fallin.tx':tc*(gridWidth+options.marginWidth) + getMarginValue(),
+					'jk_fallin.ty':mat[tc]
+				});
+				mat[tc] += $item.outerHeight(true) + options.marginHeight;
+			});
+
+			if ( options.contianerHeightControl ) {
+				$cont.height( getMaxValue(mat)-options.marginHeight );
+			}
+		}// 직사각형일 때 끝.
+
+		moveItem();
+		function moveItem(){
+			$items.each(function(i,o){
+				var $o = $(o);
+				$o.stop().css({
+					'left':$o.data('jk_fallin.tx'),
+					'top':$o.data('jk_fallin.ty')
+				})
+			});
+		}
+
 		// 가로 최소단위
-		function getItemWidth(){
+		function getGridWidth(){
 			if ( options.itemWidth ) {
 				return options.itemWidth;
 			} else {
@@ -117,7 +168,7 @@ $.fn.jk_fallin = function(opts){
 		}
 
 		// 세로 최소단위
-		function getItemHeight(){
+		function getGridHeight(){
 			if ( options.itemHeight ) {
 				return options.itemHeight;
 			} else {
@@ -135,26 +186,67 @@ $.fn.jk_fallin = function(opts){
 			}
 		}
 
-		// matrix에 빈 컬럼을 넣기 위함.
-		function getEmptyColumn () {
+		// matrix에 빈 Row를 넣기 위함.
+		function getEmptyRow () {
 			var arr = [];
 			for ( var i = 0, t = getColNum() ; i < t ; i++ ) { arr.push(0); }
 			return arr;
 		}
 
-		// 가로 column 갯수를 리턴.
-		function getColNum(){
-			return Math.floor( ( containerWidth+options.marginWidth ) / ( itemWidth+options.marginWidth ) );
+		// array 데이터중 가장 작은 수를 가진 index를 리턴.
+		function getMinIndex(arr){
+			var c = Number.MAX_VALUE, r = 0, i, len;
+			for ( i = 0, len = arr.length ; i < len ; i++ ) {
+				if ( arr[i] < c ) {
+					c = arr[i];
+					r = i;
+				}
+			}
+			return r;
 		}
+
+		function getMaxValue(arr){
+			var r = Number.MIN_VALUE, i, len;
+			for ( i = 0, len = arr.length ; i < len ; i++ ) {
+				if ( arr[i] > r ) {
+					r = arr[i];
+				}
+			}
+			return r;
+		}
+
+		// 가로 column 개수를 리턴.
+		function getColNum(){
+			var colNum = Math.floor( ( containerWidth+options.marginWidth ) / ( gridWidth+options.marginWidth ) );
+			if ( colNum > $items.length ) colNum = $items.length;
+			return colNum;
+		}
+
+		// align으로 인한 left 추가값 계산 후 리턴.
+		function getMarginValue(){
+			//align 옵션에 의한 추가 x값
+			var v = 0;
+			var remainWidth = containerWidth-(getColNum()*(gridWidth+options.marginWidth)-options.marginWidth);
+			if ( options.align == 'center' ) {
+				v = remainWidth/2;
+			} else if ( options.align == 'right' ) {
+				v = remainWidth;
+			}
+			return v;
+		}
+
 	});
 }
 
 $.fn.jk_fallin.defaults = {
+	type:'square', //'rectangle'
 	itemWidth:0,
 	itemHeight:0,
 	itemElem:'> *',
-	marginWidth:0,
-	marginHeight:0,
+	marginWidth:20,
+	marginHeight:20,
+	contianerHeightControl:true,
+	align:'center',
 	//나중에 모션 처리 할 것.
 	skipFirstMotion:false,
 	duration:300
